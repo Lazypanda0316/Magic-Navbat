6727d917e344ad629f9b9dd94ebf5814bf62f8e8
Lazypanda0316 <uganmaharjan@gmail.com>
2024-08-18 07:16:16 +0545

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation bar</title>
+    <link rel ="stylesheet" href="navigation.css">
+   <style></style>
+</head>
+<body>
+    <nav class="nav">
+        <ul>
+            <li class="list active"><a href="#"><span class="icon"><ion-icon name="home-outline"></ion-icon></span><span class="text">Home</span></a></li>
+            <li class="list"><a href="#"><span class="icon"><ion-icon name="storefront-outline"></ion-icon></span><span class="text">Shop</span></a></li>
+            <li class="list"><a href="#"><span class="icon"><ion-icon name="search-outline"></ion-icon></span><span class="text">Search</span></a></li>
+            <li class="list"><a href="#"><span class="icon"><ion-icon name="person-outline"></ion-icon></span><span class="text">Profile</span></a></li>
+        <div class="indicator"><div>
+        </ul>
+    </nav>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>   
+<script src="navigation.js">
+
+</script>
+</body>
+</html>
+ 
